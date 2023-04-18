02f5e3892311927c7290785e252bb2490e1c90ea
AbhishekKumar <abhishek.mce13@gmail.com>
2023-04-18 17:01:11 +0530

    list tuples

---
+++ list_methods.py
@@ -0,0 +1,18 @@
+numbers = [1,2,3,4,5,5,6,6,7,8,89,10]
+fruits = ['banana', 'apple', 'orange', 'pineapple', 'guava', 'peers', 'pomegranate', 'grapes']
+
+numbers.extend(fruits) #will add second list to first
+print(numbers)
+
+
+fruits.append('cherry') #will add more items at end
+print(fruits)
+
+
+print (len(fruits)) #will give the length of list
+
+fruits.insert(0, 'litchi')
+print(fruits)
+
+fruits.reverse()
+print(fruits)
---
+++ tuples.py
@@ -0,0 +1,15 @@
+gadgets = ('phone','laptop','psp','smart watch','camera')
+print(gadgets)
+
+#tuples are immutable, we cange change the values
+#tuples allow repetition of values
+#tuple object is not callable
+print(len(gadgets))
+
+print(type(gadgets))
+
+numbers = (1,2,3,4,5,6,7,8,9,8,6,4,2,1,'forty four','eighteen') #we can mix any data type in tuples
+print(numbers)
+
+print(type(numbers[3])) #will give <class 'int'>
+print(gadgets[0]) #phone
